a4abfa4038079da6480b118fd5856aa98b35f983
GABRIEL OLIVEIRA <GABRIEL.OLIVEIRA@EUAX.COM.BR>
2022-04-28 11:57:08 -0300

    public_count_message method for displaying accurate number of articles in index

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -11,6 +11,16 @@ module Visible
     def public_count
       where(status: 'public').count
     end
+
+    def public_count_message
+      if public_count == 0
+        message = "Our blog has 0 articles!"
+      elsif public_count == 1
+        message = "Our blog has 1 article!"
+      else
+        message = "Our blog has #{Article.public_count} articles and counting!"
+      end
+    end
   end
 
   def archived?
